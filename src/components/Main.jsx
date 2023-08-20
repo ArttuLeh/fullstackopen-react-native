db30a8c0caaee2e22a8018c4d4b52885523135c5
ArttuLeh <lehtovaara.arttu@gmail.com>
2023-08-20 17:32:40 +0300

    routing added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,6 @@
 import { StyleSheet, View } from 'react-native';
+import { Route, Routes, Navigate } from 'react-router-native';
+
 import RepositoryList from './RepositoryList';
 import AppBar from './AppBar';
 import theme from '../theme';
@@ -7,12 +9,17 @@ const Main = () => {
   const styles = StyleSheet.create({
     main: {
       backgroundColor: theme.main.backgroundColor,
+      flexGrow: 1,
+      flexShrink: 1,
     },
   });
   return (
     <View style={styles.main}>
       <AppBar />
-      <RepositoryList />
+      <Routes>
+        <Route path="/" element={<RepositoryList />} exact />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </View>
   );
 };
