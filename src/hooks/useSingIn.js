050d40e62e26c12f7c57c1919c4d7b3245756162
ArttuLeh <lehtovaara.arttu@gmail.com>
2023-08-31 17:56:22 +0300

    10.15: storing the access token step2

--- src/hooks/useSingIn.js
+++ src/hooks/useSingIn.js
@@ -1,16 +1,22 @@
 import { useMutation } from '@apollo/client';
+import { useApolloClient } from '@apollo/client';
+
 import { SING_IN } from '../graphql /queries';
+import useAuthStorage from './useAuthStorage';
 
 const useSingIn = () => {
   const [mutate, result] = useMutation(SING_IN);
+  const authStorage = useAuthStorage();
+  const apolloClient = useApolloClient();
 
   const singIn = async ({ username, password }) => {
     const { data } = await mutate({
       variables: { credentials: { username, password } },
     });
-    const token = data.authenticate.accessToken;
-    console.log(token);
-    return token;
+
+    await authStorage.setAccessToken(data);
+    apolloClient.resetStore();
+    return data;
   };
 
   return [singIn, result];
